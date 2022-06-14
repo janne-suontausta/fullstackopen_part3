30f55d236680d70c97d93f1f8703a584057214d3
EXT-Janne.Suontausta <EXT-Janne.Suontausta@terveystalo.com>
2022-06-14 12:27:15 +0300

    Tehtävä 3.5: puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 let persons = [
     {
         "name": "Arto Hellas",
@@ -53,6 +55,15 @@ app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(p => p.id !== id);
 
     res.status(204).end();
+})
+
+app.post('/api/persons', (req, res) => {
+    const person = req.body;
+    const id = Math.round(Math.random() * 1000000);
+    person.id = id;
+    persons = persons.concat(person)
+
+    res.json(person);
 })
 
 const PORT = 3001
